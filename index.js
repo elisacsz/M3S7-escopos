0babcf833ed122fddadb243145ea4504c4cd513d
elisacsz <elisacristina15@hotmail.com>
2022-12-18 11:40:46 -0300

    create index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var nome = "Elisa";
 {
     let nome = "Simone";
 
-    console.log("Dentro do bloco o nome é" + nome);
+    console.log("Dentro do bloco o nome é " + nome);
 }
 
-alert("Fora do bloco o nome é" + nome);
+alert("Fora do bloco o nome é " + nome);
